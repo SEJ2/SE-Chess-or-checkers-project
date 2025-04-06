d911c2b0d6754e6dc4e7d6a3fa6d281a8708595b
SEJ2 <samsontighe@gmail.com>
2025-04-06 14:41:20 -0400

    created the board and populated it with squares next on the to do list is populated the squares with pieces

--- python code/board.py
+++ python code/board.py
@@ -1,32 +1,81 @@
 import chess
 import chess.engine
-from pieces import pygame
+import pygame
+#import pieces
 
 pygame.init
 
 #setting up the screen display
 
-WITDTH, HEIGHT = 800, 800
-screen = pygame.display.set_mode([WITDTH, HEIGHT])
-font = pygame.font.Font('freesansbold.ttf', 20)
-big_font = pygame.font.Font('freesansbold.ttf', 50)
+WIDTH, HEIGHT = 400, 400
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
-fps = 80
+fps = 60
 
+# setting color options for the squares
+black = (0,0,0)
+white = (255,255,255)
 
 class Square:
-    
-    def __init__(self, color, row, collumn, piece = None):
+    def __init__(self, color, size, column, row,  piece = None):
        self.color = color
-       self.row = row
-       self.collumn = collumn
+       self.size = size
+       self.column = column 
+       self.row = row    
        self.piece = piece
+       self.size = size    
 
+    def draw_square(self, surface):
+        rectangle = pygame.Rect(self.column * self.size, self.row * self.size, self.size, self.size)
+        pygame.draw.rect(surface, self.color, rectangle)
 
+# function for drawing pieces
+   # def draw_pieces:
+   #   if self.piece == None:
+         # Square.piece = piece_object{}
 
 # creating the board 
-class board:
-     def __init__(self, size = 8):
-         (self)
+class board(chess.Board):
+     def __init__(self, surface, column = 8, Rows = 8):
+         super().__init__()
+         self.column = column
+         self.Rows = Rows
+         self.squares_coordinate = [ 
+             [None for all in range(self.column)] for all in range(self.Rows)
+             ]
+         self.square_size = WIDTH // self.column
+         self.create_squares(surface)
 
-pygame.display(board)
+     def create_squares(self,surface):
+         for column in range(self.column):
+             for row in range(self.Rows):
+               if ( column + row ) %2 == 0:
+                   color = black
+               else :
+                    color = white 
+               Square_object = Square(color, self.square_size, column, row,  piece = None)
+               Square_object.draw_square(surface) # draw the square
+                #assign it a coordinate and update the list
+               self.squares_coordinate [column][row]= Square_object
+            
+
+def main():
+    running = True
+    chessboard = board(screen)
+
+    while running: 
+        for event in pygame.event.get(): 
+        # Check for QUIT event       
+            if event.type == pygame.QUIT: 
+               running = False
+
+        screen.fill((255,255,255))
+        chessboard.create_squares(screen)
+        #updating the screen
+        pygame.display.flip()
+
+        timer.tick(fps)
+    pygame.quit()
+
+if __name__ == "__main__":
+  main()
