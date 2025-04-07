64c939e5bd8f1c15480345e607bbd7c636750757
SEJ2 <samsontighe@gmail.com>
2025-04-06 23:21:59 -0400

    finished setting up logic to assign pieces to individual sqaures, it does not work currently

--- python code/board.py
+++ python code/board.py
@@ -1,12 +1,11 @@
 import chess
 import chess.engine
 import pygame
-#import pieces
+import pieces
 
-pygame.init
+pygame.init()
 
 #setting up the screen display
-
 WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
@@ -15,24 +14,22 @@ fps = 60
 # setting color options for the squares
 black = (0,0,0)
 white = (255,255,255)
-
 class Square:
-    def __init__(self, color, size, column, row,  piece = None):
+    def __init__(self, color, size, column, row,  piece):
        self.color = color
        self.size = size
        self.column = column 
        self.row = row    
-       self.piece = piece
-       self.size = size    
+       self.piece = piece  
 
     def draw_square(self, surface):
         rectangle = pygame.Rect(self.column * self.size, self.row * self.size, self.size, self.size)
         pygame.draw.rect(surface, self.color, rectangle)
 
-# function for drawing pieces
-   # def draw_pieces:
-   #   if self.piece == None:
-         # Square.piece = piece_object{}
+    #function for drawing pieces on the board
+    def draw_pieces(self, surface):
+        if self.piece:
+            surface.blit(self.piece.image, (self.column * self.size, self.row *self.size))
 
 # creating the board 
 class board(chess.Board):
@@ -40,23 +37,50 @@ class board(chess.Board):
          super().__init__()
          self.column = column
          self.Rows = Rows
-         self.squares_coordinate = [ 
-             [None for all in range(self.column)] for all in range(self.Rows)
-             ]
+         self.squares_coordinate = [[None for all in range(self.column)] for all in range(self.Rows)]
          self.square_size = WIDTH // self.column
          self.create_squares(surface)
+         self.pieces_starting_position(surface)
+         
 
      def create_squares(self,surface):
          for column in range(self.column):
              for row in range(self.Rows):
                if ( column + row ) %2 == 0:
                    color = black
-               else :
+               else:
                     color = white 
                Square_object = Square(color, self.square_size, column, row,  piece = None)
-               Square_object.draw_square(surface) # draw the square
-                #assign it a coordinate and update the list
+               #assign it a coordinate and update the list
                self.squares_coordinate [column][row]= Square_object
+               Square_object.draw_square(surface) # draw the square
+                
+    #function for assigning pieces their initial posotion on the board
+     def pieces_starting_position(self,surface):
+        pieces.create_pieces() 
+
+        #assigning the White pieces in the backrow starting postion
+        for col, piece_object in enumerate(pieces.white_backrow):
+            piece = pieces.white_backrow[piece_object]
+            square = self.squares_coordinate[col][7]
+            square.piece = piece
+                     
+           #assigning the Pawns starting postion
+        for col in range(self.column):
+            square = self.squares_coordinate[col][6]
+            square.piece = pieces.created_piece_objects["WhitePawn"]
+
+         #assigning the Black pieces in the backrow starting postion
+        for col, piece_object in enumerate(pieces.black_backrow):
+            piece = pieces.black_backrow[piece_object]
+            square = self.squares_coordinate[col][0]
+            square.piece = piece
+                     
+           #assigning the the Pawns starting postion
+        for col in range(self.column):
+            square = self.squares_coordinate[col][1]
+            square.piece = pieces.created_piece_objects["BlackPawn"]    
+                                 
             
 
 def main():
@@ -71,10 +95,17 @@ def main():
 
         screen.fill((255,255,255))
         chessboard.create_squares(screen)
+        
+        # displaying pieces
+        for col in range(chessboard.column):
+            for row in range(chessboard.Rows):
+                square = chessboard.squares_coordinate[col][row]
+                square.draw_pieces(screen)
+
         #updating the screen
         pygame.display.flip()
-
         timer.tick(fps)
+        
     pygame.quit()
 
 if __name__ == "__main__":
--- python code/pieces.py
+++ python code/pieces.py
@@ -1,22 +1,22 @@
-# game logic for chess prject
+# game logic for chess project
 import pygame
 import os 
 
 pygame.init()
 
-
 # creating the pieces super class
 class chessPiece: 
 
-    def __init__(self, color, Type, worth, image_path):
+    def __init__(self, color, piece_type, worth, image_path):
         self.color = color
-        self.Type = Type
+        self.Type = piece_type
         self.worth = worth 
+
         try:
               self.image = pygame.image.load(image_path)
-              print(f"{color}_{Type} loaded successfuly")
+              print(f"{color}_{piece_type} loaded successfully")
         except pygame.error: 
-              print(f"failed to load {Type}_{image_path}")  
+              print(f"failed to load {piece_type}") 
 
     
 # driver code that initialize the pieces of in both colors
@@ -26,22 +26,43 @@ colors = ["White", "Black"]
 file_directory = os.path.dirname((os.path.abspath(__file__)))
 folder = os.path.join(file_directory, "images/")
 
-# dictionary to hold objects of chesspice class
-types_of_object = {}
-
+# dictionary to hold objects of Chesspiece class
+created_piece_objects = {}
+backrow = ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"]
+white_backrow = {}
+black_backrow = {}
  # creating one object piece in both colors
 
-
 def create_pieces():
- for color in colors:
-      for piece in list_of_pieces:
-          piece_type = f"{color}{piece}"
-          image_path = os.path.join(folder, f"{color}_{piece}.png")
-          piece_object = chessPiece(color, piece, 0, image_path)
+    for color in colors:
+         for piece in list_of_pieces:
+             piece_type = f"{color}{piece}"
+             image_path = os.path.join(folder, f"{color}_{piece}.png")
+
+          # giving each piece their points
+             if piece == "Pawn":
+               worth = 1
+             elif  piece == "Knight" or piece =="Bishop":
+              worth = 3
+             elif  piece == "Rook":
+              worth = 5
+             elif  piece == "Queen":
+              worth = 9 
+             else:  
+              worth = 1000
+              #creating the chesspiece objects
+             piece_object = chessPiece(color, piece, worth, image_path)
            
            # updating the dictionary each time an object is created
-          types_of_object[piece_type] = piece_object
+             created_piece_objects[piece_type] = piece_object
 
-              
+    #updating the backrows of each color
+    for piece in backrow:
+        white_backrow[piece] = created_piece_objects[f"White{piece}"]
+        black_backrow[piece]=  created_piece_objects[f"Black{piece}"]
+
+        
+          
 # calling function to create pieces
-create_pieces() 
+create_pieces()
+
